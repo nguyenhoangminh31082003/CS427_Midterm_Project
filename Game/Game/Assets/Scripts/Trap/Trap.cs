2b07a09e60587b20763b10cff2ba1947bc8a506a
phucst123 <huuphucst123@gmail.com>
2024-06-28 21:23:17 +0700

    update traps

--- Game/Game/Assets/Scripts/Trap/Trap.cs
+++ Game/Game/Assets/Scripts/Trap/Trap.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public abstract class Trap : MonoBehaviour
@@ -6,21 +7,24 @@ public abstract class Trap : MonoBehaviour
     public float triggerInterval = 2.0f;  // Time between triggers
     private float timer = 0.0f;           // Timer to keep track of interval
     public bool isTriggered;              // Whether the trap is currently triggered
+    private bool damaged;
+    private bool isIn;
     private Animator _animator;
     private string _currentState;
-
     protected abstract string AttackState { get; }
     protected abstract string SleepState { get; }
 
-    // Start is called before the first frame update
-    protected virtual void Start()
-    {
-        isTriggered = false;
+    void Awake() {
         _animator = GetComponent<Animator>();
     }
+    void Start()
+    {
+        isIn = false;
+        isTriggered = false;
+        damaged = false;
+    }
 
-    // Update is called once per frame
-    protected virtual void Update()
+    void Update()
     {
         timer += Time.deltaTime;
         if (timer >= triggerInterval)
@@ -37,46 +41,27 @@ public abstract class Trap : MonoBehaviour
         {
             ChangeAnimationState(SleepState);
             isTriggered = false;
+            damaged = false;
         }
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("OnCollisionEnter2D");
-        Debug.Log(collision.gameObject.name);
-        if (isTriggered && collision.gameObject.CompareTag("Player"))
+        if (isTriggered && isIn && !damaged)
         {
-            Debug.Log("Deal Damage from enter");
-            // You can add damage logic or other effects here if needed
+            damaged = true;
+            Debug.Log("Deal Damage to player");
+            // Call manager to deal damage
+            
         }
     }
 
-    private void OnCollisionStay2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("OnCollisionStay2D");
-        Debug.Log(collision.gameObject.name);
-        if (isTriggered && collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Deal Damage from stay");
-        }
+        isIn = true;
     }
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        Debug.Log("OnCollisionExit2D");
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            // Additional logic can be added here if needed
-        }
+    void OnTriggerExit2D(Collider2D other)
+    {       
+        isIn = false;
     }
-
-    void OnTriggerEnter2D(Collider2D collider)
-    {
-        Debug.Log("OnTriggerEnter2D");
-        Debug.Log(collider.gameObject.name);
-    }
-
     private void ChangeAnimationState(string newState)
     {
         if (newState == _currentState)
