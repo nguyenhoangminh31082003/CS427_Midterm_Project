96fad246f67eb65b21d2f3f0c30fa8b434ac041b
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2024-06-28 18:17:44 +0700

    Update MainCharacter script

--- Game/Game/Assets/Scripts/MainCharacter.cs
+++ Game/Game/Assets/Scripts/MainCharacter.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MainCharacter : MonoBehaviour
 {
 
+    const double WEIGHT_LIMIT = 1E8;
+
     private SpriteRenderer spriteRender;
     private Rigidbody2D rigidBody2D;
-    private float speedX, speedY;
-    private float weight;
+    private double speedX, speedY;
+    private double weight;
+    private int liveCount;
 
     // Start is called before the first frame update
     void Start()
@@ -18,44 +22,39 @@ public class MainCharacter : MonoBehaviour
         this.weight = 1;
         this.speedX = 0;
         this.speedY = 0;
+        this.liveCount = 5;
+    }
+
+    void UpdateVelocity()
+    {
+        bool rightArrow = Input.GetKey(KeyCode.RightArrow),
+             leftArrow = Input.GetKey(KeyCode.LeftArrow),
+             upArrow = Input.GetKey(KeyCode.UpArrow),
+             downArrow = Input.GetKey(KeyCode.DownArrow);
+        this.speedX = 0;
+        this.speedY = 0;
+        if (leftArrow)
+            this.speedX += 1;
+        if (rightArrow)
+            this.speedX -= 1;
+        if (upArrow)
+            this.speedY += 1;
+        if (downArrow)
+            this.speedY -= 1;
+        double percentage = 1 - weight / WEIGHT_LIMIT;
+        this.speedX *= percentage;
+        this.speedY *= percentage;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool rightArrow = Input.GetKey(KeyCode.RightArrow),
-             leftArrow = Input.GetKey(KeyCode.LeftArrow),
-             upArrow   = Input.GetKey(KeyCode.UpArrow),
-             downArrow = Input.GetKey(KeyCode.DownArrow);
-        if (leftArrow && !(rightArrow || upArrow || downArrow))
-        {
-            this.speedX = -1;
-            this.speedY = 0;
-        } 
-        else if (rightArrow && !(leftArrow || upArrow || downArrow))
-        {
-            this.speedX = 1;
-            this.speedY = 0;
-        } 
-        else if (upArrow && !(leftArrow || rightArrow || downArrow)) 
-        {
-            this.speedX = 0;
-            this.speedY = 1;
-        }
-        else if (downArrow && !(leftArrow || rightArrow || upArrow))
-        {
-            this.speedX = 0;
-            this.speedY = -1;
-        }
-        else
-        {
-            this.speedX = 0;
-            this.speedY = 0;
-        }
+        UpdateVelocity();
     }
 
     private void FixedUpdate()
     {
-        this.rigidBody2D.velocity = new Vector2(this.speedX, this.speedY);
+        //GameManager.setPlayerVelocity(this.speedX, this.speedY);
+        this.rigidBody2D.velocity = new Vector2((float)this.speedX, (float)this.speedY);
     }
 }
