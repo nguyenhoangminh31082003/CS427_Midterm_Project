92a65ccd2d204f3910ee8ad79955d9833504a007
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2024-07-01 20:08:38 +0700

    Reorganize files

--- Game/Game/Assets/Scripts/MainCharacter/Weapons/Weapon.cs
+++ Game/Game/Assets/Scripts/MainCharacter/Weapons/Weapon.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sword : MonoBehaviour
+public class Weapon : MonoBehaviour
 {
+
+    [SerializeField] protected int number;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.number = 0;
     }
 
     // Update is called once per frame
