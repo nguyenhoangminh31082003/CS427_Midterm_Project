81e486608fb95725247222e9cfc6518ded9f4f9e
Nguyễn Hoàng Minh <nguyen31hoang08minh2003@gmail.com>
2024-07-01 20:32:56 +0700

    Add images for bow and sword

--- Game/Game/Assets/Scripts/MainCharacter/Weapons/Weapon.cs
+++ Game/Game/Assets/Scripts/MainCharacter/Weapons/Weapon.cs
@@ -6,11 +6,16 @@ public class Weapon : MonoBehaviour
 {
 
     [SerializeField] protected int number;
+    [SerializeField] protected bool currentlyUsed;
 
     // Start is called before the first frame update
     void Start()
     {
         this.number = 0;
+
+        this.currentlyUsed = false;
+
+        this.gameObject.SetActive(this.currentlyUsed);
     }
 
     // Update is called once per frame
