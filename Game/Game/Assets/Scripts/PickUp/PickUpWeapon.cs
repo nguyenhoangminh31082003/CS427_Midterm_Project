445fdde93c5d9df6c507945d943e79c5e60f1445
phucst123 <huuphucst123@gmail.com>
2024-07-07 01:03:52 +0700

    add curve appear for weapon item

--- Game/Game/Assets/Scripts/PickUp/PickUpWeapon.cs
+++ Game/Game/Assets/Scripts/PickUp/PickUpWeapon.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +5,19 @@ using UnityEngine;
 public class PickUpWeapon : Interactable
 {
     [SerializeField] private ItemWeaponType weaponType;
+    [SerializeField] private AnimationCurve animCurve;
+    [SerializeField] private float heightY = 1.5f;
+    [SerializeField] private float popDuration = 0.5f;
     public enum ItemWeaponType {
         Sword,
         Bow,
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(AnimCurveSpawnRoutine());
+
     }
     protected override void Interact()
     {
@@ -21,4 +30,24 @@ public class PickUpWeapon : Interactable
         return weaponType;
     }
 
+    private IEnumerator AnimCurveSpawnRoutine() {
+        Vector2 startPoint = transform.position;
+        float randomX = transform.position.x + Random.Range(-2f, 2f);
+        float randomY = transform.position.y + Random.Range(-1f, 1f);
+
+        Vector2 endPoint = new Vector2(randomX, randomY);
+
+        float timePassed = 0f;
+
+        while (timePassed < popDuration)
+        {
+            timePassed += Time.deltaTime;
+            float linearT = timePassed / popDuration;
+            float heightT = animCurve.Evaluate(linearT);
+            float height = Mathf.Lerp(0f, heightY, heightT);
+
+            transform.position = Vector2.Lerp(startPoint, endPoint, linearT) + new Vector2(0f, height);
+            yield return null;
+        }
+    }
 }
