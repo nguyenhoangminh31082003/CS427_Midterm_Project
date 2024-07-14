78e996188f4fd19f5e59004c046a45961f868652
nmvinhdl1215 <nmvinhdl1215@gmail.com>
2024-07-14 11:01:40 +0700

    - edit other audio component

--- Game/Game/Assets/Scripts/StoryItem.cs
+++ Game/Game/Assets/Scripts/StoryItem.cs
@@ -5,8 +5,14 @@ public class StoryItem : Interactable
     //public string storyPlot;
 
     [SerializeField] private Dialogue dialogue;
+    public string backgroundTrack = "";
     protected override void Interact()
     {
+        if (backgroundTrack.Length > 0)
+        {
+            AudioManager.Instance.currentTrack = backgroundTrack;
+            AudioManager.Instance.PlayMusic(backgroundTrack);
+        }
         dialogueManager.StartDialogue(dialogue);
     }
 
