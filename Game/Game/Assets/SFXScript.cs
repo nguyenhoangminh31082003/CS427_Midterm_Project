1dd9a9ab1da1a6f33b77b2a5824d4701893f3a32
phatalways_sleeping <trankim147@gmail.com>
2024-07-17 22:04:12 +0700

    slider

--- Game/Game/Assets/SFXScript.cs
+++ Game/Game/Assets/SFXScript.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SFXScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+ // Start is called before the first frame update
     void Start()
     {
-        
+        var slider = GetComponentInChildren<Slider>();
+        var toggle = GetComponentInChildren<Toggle>();
+
+        toggle.onValueChanged.AddListener(MuteMusic);
+        slider.onValueChanged.AddListener(SetVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void MuteMusic(bool isMuted)
+    {
+        AudioListener.volume = isMuted ? 0 : 0.5f;
+        GetComponentInChildren<Slider>().interactable = !isMuted;
+        GetComponentInChildren<Slider>().value = isMuted ? 0 : 0.5f;
+    }
+
+    public void SetVolume(float volume)
+    {
+        if (GetComponentInChildren<Toggle>().isOn)
+        {
+            AudioListener.volume = volume;
+        }
     }
 }
